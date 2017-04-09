31bcb8f8aae8cc920894e67afd471313bd0fb73c
Jose Lorenzo <josee.loren@gmail.com>
2017-04-09 19:23:29 +0000

    Including compile script and source code

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <wx/wx.h>
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit() wxOVERRIDE;
+};
+
+class MyFrame : public wxFrame {
+public:
+  MyFrame();
+  void OnClick(wxCommandEvent& event);
+};
+
+class MyModalDialog : public wxDialog
+{
+public:
+
+  MyModalDialog(wxWindow *parent = NULL);
+
+  void OnInit(wxInitDialogEvent& event);
+  
+private:
+    wxDECLARE_EVENT_TABLE();
+};
+
+
+BEGIN_EVENT_TABLE( MyModalDialog, wxDialog )
+EVT_INIT_DIALOG( MyModalDialog::OnInit )
+END_EVENT_TABLE()
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit()
+{
+  MyFrame* frame= new MyFrame();
+  frame -> Show();
+    return true;
+}
+
+MyModalDialog::MyModalDialog( wxWindow* parent )
+  : wxDialog(parent,wxID_ANY,  wxString(_T("Modal dialog") ))
+{
+}
+
+void MyModalDialog::OnInit(wxInitDialogEvent& WXUNUSED(event))
+{
+  if ( !IsModal() )
+    wxMessageBox(_T("not modal"));
+}
+
+MyFrame::MyFrame() : wxFrame(NULL, wxID_ANY, wxT("Prueba"), wxDefaultPosition, wxSize(200,150))
+{
+  wxPanel *panel = new wxPanel(this, wxID_ANY);
+
+  wxButton *button = new wxButton(panel, wxID_EXIT, wxT("Modal"),
+				  wxPoint(20,20));
+  Connect(wxID_EXIT, wxEVT_COMMAND_BUTTON_CLICKED,
+	  wxCommandEventHandler(MyFrame::OnClick));
+  button -> SetFocus();
+  Centre();
+  
+}
+
+void MyFrame::OnClick(wxCommandEvent& WXUNUSED(event))
+{
+    MyModalDialog d;
+
+    int a = d.ShowModal();  
+}
