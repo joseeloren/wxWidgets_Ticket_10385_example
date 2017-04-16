39a689d9d2789936aa1b623eab55c1001a8adfa2
Jose <isleo11@gmail.com>
2017-04-16 10:51:43 +0100

    EndModal() at the end of handler

--- main.cpp
+++ main.cpp
@@ -46,10 +46,9 @@ MyModalDialog::MyModalDialog( wxWindow* parent )
 void MyModalDialog::OnInit(wxInitDialogEvent& WXUNUSED(event))
 {
 	printf("On Init\n");
-	EndModal(wxID_OK);
 	if ( !IsModal() )
     wxMessageBox(_T("not modal"));
-  
+	EndModal(wxID_OK);  
 }
 
 MyFrame::MyFrame() : wxFrame(NULL, wxID_ANY, wxT("Prueba"), wxDefaultPosition, wxSize(200,150))
